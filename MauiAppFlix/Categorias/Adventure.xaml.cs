dee897941605ca096047ee201fda43fc3eddf1e9
Usuario <Usuario@CPD01-M20>
2024-05-02 15:49:04 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFlix/Categorias/Adventure.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppFlix.Categorias;
+
+public partial class Adventure : ContentPage
+{
+	public Adventure()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppFlix/Categorias/Documentary.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppFlix.Categorias;
+
+public partial class Documentary : ContentPage
+{
+	public Documentary()
+	{
+		InitializeComponent();
+	}
+}
